0a52fdcb193699b1da9397d7f644e450d59a0e7f
LeaderMr20 <118063170+LeaderMr20@users.noreply.github.com>
2026-03-04 00:15:31 +0300

    Add backend/app/Http/Middleware/CheckRole.php

---
+++ backend/app/Http/Middleware/CheckRole.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckRole
+{
+    public function handle(Request $request, Closure $next, ...$roles)
+    {
+        $user = $request->user();
+
+        if (!$user || !in_array($user->role, $roles)) {
+            return response()->json([
+                'message' => 'ليس لديك الصلاحية للقيام بهذا الإجراء',
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
